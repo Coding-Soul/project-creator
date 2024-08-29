f1090ccb8352cb1f85d2289e1d921198dd78cbe9
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-08-29 19:54:10 +0200

    feat: opening exact project

--- core/commands/project.py
+++ core/commands/project.py
@@ -6,8 +6,8 @@ from core import logic
 from core import config
 from utils import colors
 
-
 app = typer.Typer()
+
 
 @app.command()
 def create():
@@ -15,31 +15,30 @@ def create():
     name = input('> ')
     typer.echo('Language / Stack')
     project_type = input('> ')
-    
-    
+
     types = {
         "python": ProjectType.PYTHON,
         "py": ProjectType.PYTHON
     }
-    
+
     for key, value in types.items():
         if project_type.lower() == 'list':
-            
+
             msg = "List of supported languages\n"
-            
+
             typer.echo(msg)
             break
-            
+
         elif project_type.lower() == key:
             logic.create_project(name=name, type=value)
-            os.system(f'code {config.read("app")["projects-path"]}')
+            os.system(f'code {config.read("app")["projects-path"]}/{name}')
+
 
 @app.command()
 def list():
     listed_directory = os.listdir(path=config.read('app')['projects-path'])
     output = "Listed Directorys:\n\n"
-    
+
     for item in listed_directory:
         output += f"{item}\n"
     typer.echo(output)
-    
